f725b48bb3a20e8dfc7b7501702b5f2694abac1f
Olivier Revollat <revollat@gmail.com>
2017-07-11 16:22:20 +0200

    entité livre

---
+++ sfapp/src/AppBundle/Controller/LivreController.php
@@ -0,0 +1,136 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\Livre;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Livre controller.
+ *
+ * @Route("livre_admin")
+ */
+class LivreController extends Controller
+{
+    /**
+     * Lists all livre entities.
+     *
+     * @Route("/", name="livre_admin_index")
+     * @Method("GET")
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $livres = $em->getRepository('AppBundle:Livre')->findAll();
+
+        return $this->render('livre/index.html.twig', array(
+            'livres' => $livres,
+        ));
+    }
+
+    /**
+     * Creates a new livre entity.
+     *
+     * @Route("/new", name="livre_admin_new")
+     * @Method({"GET", "POST"})
+     */
+    public function newAction(Request $request)
+    {
+        $livre = new Livre();
+        $form = $this->createForm('AppBundle\Form\LivreType', $livre);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($livre);
+            $em->flush();
+
+            return $this->redirectToRoute('livre_admin_show', array('id' => $livre->getId()));
+        }
+
+        return $this->render('livre/new.html.twig', array(
+            'livre' => $livre,
+            'form' => $form->createView(),
+        ));
+    }
+
+    /**
+     * Finds and displays a livre entity.
+     *
+     * @Route("/{id}", name="livre_admin_show")
+     * @Method("GET")
+     */
+    public function showAction(Livre $livre)
+    {
+        $deleteForm = $this->createDeleteForm($livre);
+
+        return $this->render('livre/show.html.twig', array(
+            'livre' => $livre,
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Displays a form to edit an existing livre entity.
+     *
+     * @Route("/{id}/edit", name="livre_admin_edit")
+     * @Method({"GET", "POST"})
+     */
+    public function editAction(Request $request, Livre $livre)
+    {
+        $deleteForm = $this->createDeleteForm($livre);
+        $editForm = $this->createForm('AppBundle\Form\LivreType', $livre);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isSubmitted() && $editForm->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('livre_admin_edit', array('id' => $livre->getId()));
+        }
+
+        return $this->render('livre/edit.html.twig', array(
+            'livre' => $livre,
+            'edit_form' => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Deletes a livre entity.
+     *
+     * @Route("/{id}", name="livre_admin_delete")
+     * @Method("DELETE")
+     */
+    public function deleteAction(Request $request, Livre $livre)
+    {
+        $form = $this->createDeleteForm($livre);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($livre);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('livre_admin_index');
+    }
+
+    /**
+     * Creates a form to delete a livre entity.
+     *
+     * @param Livre $livre The livre entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm(Livre $livre)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('livre_admin_delete', array('id' => $livre->getId())))
+            ->setMethod('DELETE')
+            ->getForm()
+        ;
+    }
+}
---
+++ sfapp/src/AppBundle/Entity/Livre.php
@@ -0,0 +1,86 @@
+<?php
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="livre")
+ */
+class Livre
+{
+    /**
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $titre;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $description;
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set titre
+     *
+     * @param string $titre
+     *
+     * @return Livre
+     */
+    public function setTitre($titre)
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+    /**
+     * Get titre
+     *
+     * @return string
+     */
+    public function getTitre()
+    {
+        return $this->titre;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     *
+     * @return Livre
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+}
