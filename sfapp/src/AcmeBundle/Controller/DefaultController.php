16daa6d015419460f474ce36c9769ed24d5533a3
Olivier Revollat <revollat@gmail.com>
2017-07-31 17:12:25 +0200

    Ajout simple route avec prefix /acme et template à AcmeBundle

---
+++ sfapp/src/AcmeBundle/Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace AcmeBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('AcmeBundle:Default:index.html.twig');
+    }
+}
